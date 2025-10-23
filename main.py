d7ed384a190c8697274dacba44aa20d4e8cef3fe
Amit Berg <bamit127@gmail.com>
2025-10-23 23:34:30 +0300

    added frame rate limitation (60)

--- main.py
+++ main.py
@@ -12,13 +12,17 @@ def main():
 
     running = True
 
+    clock = pygame.time.Clock()
+    dt = 0
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                runnig = False
+                running = False
             
         screen.fill("Black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
