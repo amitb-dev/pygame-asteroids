b771e6c1e11421964e72f939a26a63a1cd0b6021
Amit Berg <bamit127@gmail.com>
2025-10-24 04:38:14 +0300

    feat: shooting cd

--- player.py
+++ player.py
@@ -7,6 +7,8 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
+
 
     # in the player class
     def triangle(self):
@@ -36,6 +38,8 @@ class Player(CircleShape):
             self.move(-1 * dt)
         if keys[pygame.K_SPACE]:
             self.shoot()
+
+        self.shoot_timer -= dt
         
 
     def move(self, dt):
@@ -43,7 +47,11 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        shot = Shot(self.position.x, self.position.y)
-        forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        shot.velocity = forward * PLAYER_SHOOT_SPEED
+        if self.shoot_timer <= 0:
+            shot = Shot(self.position.x, self.position.y)
+            
+            forward = pygame.Vector2(0, 1).rotate(self.rotation)
+            shot.velocity = forward * PLAYER_SHOOT_SPEED
+            
+            self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         
