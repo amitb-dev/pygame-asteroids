e1658590ad22249543a0db2cc7825248a408027a
Amit Berg <bamit127@gmail.com>
2025-10-24 03:45:50 +0300

    feat: groups

--- main.py
+++ main.py
@@ -16,18 +16,26 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             
-        player.update(dt)
-    
+        updatable.update(dt)
+
         screen.fill("Black")
-        player.draw(screen)
-        
+
+        for entity in drawable:
+            entity.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
